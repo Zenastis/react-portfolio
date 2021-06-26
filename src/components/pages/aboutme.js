ca5462981529e03aea6b74e1d3bd9cc83096ee9c
samuel mcswain <samuelmcswain@hotmail.com>
2021-06-26 12:55:09 -0400

    Aboutme complete

--- src/components/pages/aboutme.js
+++ src/components/pages/aboutme.js
@@ -5,8 +5,62 @@ import React from "react"
 
 
 
-function aboutMe () {
+function Aboutme() {
     return (
-        
+        <div>
+            <h1>About Me</h1>
+            <div class="aboutme">
+                <div>
+                    <img></img>
+                </div>
+
+                <p> I was born in Spartanburg SC on September 15, 1990. I am the second of two siblings,
+                    my older sister is Brittany.
+                    My mother was resturaunt owner and my step father was a grading company owner.  Together they operated a marina in Salisbury Nc 
+                    on High Rock Lake.  I spent my formative years in Salisbury, highschool and a brief time after.  
+                </p>
+
+                <p> During high school, I got my first taste of leadership when i made team co captain of the swim team both my junior and senior years.
+                    During those two years I was able to make it to the regional and state championship both years through hard, commitment, and dedication to
+                    the sport I loved.  
+                </p>
+
+                <p> After highschool, I had recieved my letter of acceptance to Catawba College, with an athletic swimming grant, in Salisbury. 
+                    I spent a year there before ultimately leaving 
+                    and joining the workforce.  My first full time job was at the MillerCoors distributor named "United Beverage", I spent 3 years there before moving back
+                    to Spartanburg SC to be closer to the rest of my family.  The job i took at the BMW manufacturing facility lasted 2 years before I went back to the
+                    alchohol business, this time with "Budweiser of Spartanburg".  I stayed there until i met my now wife in 2017.
+                </p>
+
+                <p> It was around 2017, when I met my wife, that I started thinking about going back to school and getting my degree.  This seemed like a daunting 
+                    task, as id of had to go back as a part time student and of consequence that would have taken the better part of a decade to do.  An amount of 
+                    time i would of not liked to dedicate to one task.  So i began investigating other paths and came across the term "Bootcamp" for certificates and hands
+                    on training.  So, I did some discerning of what i thought id actually like to do as a career and whether or not i'd enjoy it.  It was at
+                    this point I beleive i had my AHA! moment.  Ive always enjoyed the tech world, whether it was gaming or just learning about how programs or
+                    software worked.
+
+                </p>
+
+                <p> After I moved to Charlotte, before my wife and I were married, I went to work for "Adams Beverages of NC", where I am currently
+                    employed as a Sales Team Lead.  My current position duties include but are not limited to time management and training of the junior salesmen on my team, the 
+                    execution of product sales to the accounts assigned which include Harris Teeter, FoodLion, Walmart, CVS, Walgreens, and Compare Foods.  Duties also include
+                    product knowledge, inventory management, product quality assurance and most importantly, keeping a great relationship maintained with accounts.
+                </p>
+
+                <p> I first started to think about web development as my future while at my current job.  I want more out of life for my family and for myself in addition to the personal and 
+                    professional growth it provides.  I have always had a love of technology and how it continues to evolve and the more I started to think about it, the more it made sense to make a change.   
+                    I enrolled in the full stack web development certificate program at the University of North Carolina at Charlotte and
+                    that is where my passion for coding truly grew as the possibilities are truly endless.
+                </p>
+
+
+            </div>
+        </div>
     )
 }
+
+
+
+
+
+export default Aboutme
