5d32c4b406720977351f81b5e6d93e6d3ba1b0e3
samuel mcswain <samuelmcswain@hotmail.com>
2021-06-26 13:10:26 -0400

    pagefooter complete

---
+++ src/components/pagefooter.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Row } from "reactstrap"
+
+function Pagefooter() {
+    return (
+        <div>
+            <Row>
+                <h3 className="footnote">Samuel Mcswain 2021</h3>
+            </Row>
+            <Row className="footlinks">
+                <a href="https://www.linkedin.com/in/samuel-mcswain-8009b0202/"> Linkedin </a>
+
+                <a href="https://github.com/Zenastis"> Github </a>
+            </Row>
+
+        </div>
+    )
+}
+
+export default Pagefooter
