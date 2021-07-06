9604ccced5acf2ec9e6ba3851286d03790a90138
samuel mcswain <samuelmcswain@hotmail.com>
2021-07-06 17:39:55 -0400

    pages components

--- src/components/pages/aboutme.js
+++ src/components/pages/aboutme.js
@@ -19,23 +19,23 @@ function Aboutme() {
                     on High Rock Lake.  I spent my formative years in Salisbury, highschool and a brief time after.  
                 </p>
 
-                <p> During high school, I got my first taste of leadership when i made team co captain of the swim team both my junior and senior years.
-                    During those two years I was able to make it to the regional and state championship both years through hard, commitment, and dedication to
+                <p> During high school, I got my first taste of leadership when I made team co captain of the swim team both my junior and senior years.
+                    During those two years I was able to make it to the regional and state championship both years through hard work, commitment, and dedication to
                     the sport I loved.  
                 </p>
 
                 <p> After highschool, I had recieved my letter of acceptance to Catawba College, with an athletic swimming grant, in Salisbury. 
-                    I spent a year there before ultimately leaving 
-                    and joining the workforce.  My first full time job was at the MillerCoors distributor named "United Beverage", I spent 3 years there before moving back
-                    to Spartanburg SC to be closer to the rest of my family.  The job i took at the BMW manufacturing facility lasted 2 years before I went back to the
+                    I spent a year there before ultimately leaving and joining the workforce.  My first full time job was at the 
+                    MillerCoors distributor named "United Beverage", I spent 3 years there before moving back
+                    to Spartanburg SC to be closer to the rest of my family.  The job I took at the BMW manufacturing facility lasted 2 years before I went back to the
                     alchohol business, this time with "Budweiser of Spartanburg".  I stayed there until i met my now wife in 2017.
                 </p>
 
                 <p> It was around 2017, when I met my wife, that I started thinking about going back to school and getting my degree.  This seemed like a daunting 
                     task, as id of had to go back as a part time student and of consequence that would have taken the better part of a decade to do.  An amount of 
-                    time i would of not liked to dedicate to one task.  So i began investigating other paths and came across the term "Bootcamp" for certificates and hands
-                    on training.  So, I did some discerning of what i thought id actually like to do as a career and whether or not i'd enjoy it.  It was at
-                    this point I beleive i had my AHA! moment.  Ive always enjoyed the tech world, whether it was gaming or just learning about how programs or
+                    time i would of not liked to dedicate to one task.  So I began investigating other paths and came across the term "Bootcamp" for certificates and hands
+                    on training.  So, I did some discerning of what I thought id actually like to do as a career and whether or not i'd enjoy it.  It was at
+                    this point I beleive I had my AHA! moment.  Ive always enjoyed the tech world, whether it was gaming or just learning about how programs or
                     software worked.
 
                 </p>
@@ -49,7 +49,7 @@ function Aboutme() {
                 <p> I first started to think about web development as my future while at my current job.  I want more out of life for my family and for myself in addition to the personal and 
                     professional growth it provides.  I have always had a love of technology and how it continues to evolve and the more I started to think about it, the more it made sense to make a change.   
                     I enrolled in the full stack web development certificate program at the University of North Carolina at Charlotte and
-                    that is where my passion for coding truly grew as the possibilities are endless.
+                    that is where my passion for coding grew as the possibilities for growth are endless.
                 </p>
 
 
--- src/components/pages/projects.js
+++ src/components/pages/projects.js
@@ -1,5 +1,4 @@
 import React from "react"
-import "../../App.css";
 import charlotteskyline from "../../assets/images/charlotteskyline.jpeg"
 
 
@@ -8,14 +7,14 @@ function Projects () {
         
         <div>
               <div>
-                <h1>Projects Page</h1>
+                <h1> Projects </h1>
                 <div class="wrapper">
                   <div class="wrapper1">
                   
                     <div class="Charlotte">
                       
                         <a href="https://kallandan.github.io/01P-Charlotte_Sights_And_Lights/">
-                        <img className="charlotteskyline" src={charlotteskyline} alt="csl"></img>
+                        <img className="charlotteskyline" src={charlotteskyline} alt="charlotte_sights_and_lights"></img>
                         </a>
 
                         <h1 class="header1"> Charlotte Sights And Lights </h1>
@@ -29,6 +28,7 @@ function Projects () {
                         <a href="https://ecomercebeerbox.herokuapp.com/">
                             <img className="beerbox" src="https://raw.githubusercontent.com/Zenastis/eComerce_Beer_Box/main/public/pic/Beer-Sampler-Gift-Select_large.jpg" alt="beerbox"></img>
                         </a>
+                        
                         <h1 class="header2"> Ecommerce Beer Box </h1>
                         
                         <p class="link2">- Repository Link: https://github.com/Zenastis/eComerce_Beer_Box</p>
@@ -40,9 +40,10 @@ function Projects () {
                         <a href="https://hidden-bastion-49542.herokuapp.com/">
                             <img className="notetaker" src="https://images.ctfassets.net/lzny33ho1g45/5iJ10OKtmYa4BZpYvhb2xw/3d30495ba0fb4603cc957a25193df4fd/best-note-taking-apps-00-hero__2_.png?w=1520&fm=jpg&q=30&fit=thumb&h=760" alt="notetakerapp"></img>
                         </a>
+                        
                         <h1 class="header3"> Note Taking App </h1>
                         
-                        <p class="para4">- Repository Link: https://github.com/Zenastis/express.js_notetaker </p>
+                        <p class="link3">- Repository Link: https://github.com/Zenastis/express.js_notetaker </p>
                         
                         
                         </div>
