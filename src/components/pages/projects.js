f6475bdfe180863b293ecc15e1c84c513ec2489c
AClineff <ajclinef@gmail.com>
2021-07-31 15:40:46 -0400

    Update projects.js
    disclaimer: I think this repo might be out of date, so I don't recommend merging this.
    
    I wanted to point out a good opportunity for demonstrating actual coding skills as opposed to demonstrating an HTML wall.  There's other opportunities for this across this repo.  This is by no means necessary, but something to keep in mind.  And if nothing else, good practice!
    
    Instead

--- src/components/pages/projects.js
+++ src/components/pages/projects.js
@@ -1,7 +1,27 @@
 import React from "react"
+import Button from '@material-ui/core/Button';
 import charlotteskyline from "../../assets/images/charlotteskyline.jpeg"
 
-  
+const projects = [
+    {
+        class: 'charlotteskyline',
+        sourceImage: charlotteskyline,
+        sourceImageAlt: 'charlotte_sights_and_lights',
+        title: 'Charlotte Sights and Lights',
+        description: 'Charlotte Sights And Lights',
+        codeLink: 'https://kallandan.github.io/01P-Charlotte_Sights_And_Lights/',
+        repoLink: 'https://github.com/Kallandan/01P-Charlotte_Sights_And_Lights',
+    },
+    {
+        class: 'beerbox',
+        sourceImage: 'https://raw.githubusercontent.com/Zenastis/eComerce_Beer_Box/main/public/pic/Beer-Sampler-Gift-Select_large.jpg',
+        sourceImageAlt: 'Beerbox',
+        title: 'BeerBox',
+        description: 'Ecommerce Beer Box',
+        codeLink: 'https://ecomercebeerbox.herokuapp.com/',
+        repoLink: 'https://github.com/Zenastis/eComerce_Beer_Box'
+    },
+] 
   
 function Projects () {
     return(
@@ -11,39 +31,17 @@ function Projects () {
                 <h1> Projects </h1>
                 <div>
                     <div>
-                        <div>
-                            <a href="https://kallandan.github.io/01P-Charlotte_Sights_And_Lights/">
-                            <img className="charlotteskyline" src={charlotteskyline} alt="charlotte_sights_and_lights"></img>
-                            </a>
-                            <h1 class="header1"> Charlotte Sights And Lights </h1>
-                            <p class="link1">- Repository Link: https://github.com/Kallandan/01P-Charlotte_Sights_And_Lights</p>          
-                        </div>
-
-                        <div>
-                            <a href="https://ecomercebeerbox.herokuapp.com/">
-                                <img className="beerbox" src="https://raw.githubusercontent.com/Zenastis/eComerce_Beer_Box/main/public/pic/Beer-Sampler-Gift-Select_large.jpg" alt="beerbox"></img>
-                            </a>
-                            <h1 class="header2"> Ecommerce Beer Box </h1>
-                            <p class="link2">- Repository Link: https://github.com/Zenastis/eComerce_Beer_Box</p>
-                        </div>
-
-                        <div>
-                            <a href="https://safe-cliffs-00867.herokuapp.com/">
-                                <img className="textadventure" src="https://i.ytimg.com/vi/yhfTmAOgb5E/hqdefault.jpg" alt="Castle of Madness"></img>
-                            </a>
-                            <h1 class="header2"> Text Adventure Game "Castle of Madness" </h1>
-                            <p class="link4">- Repository Link: https://github.com/jdrabot/text-adventure-game</p>
-                            
-                        </div>
-
-                        <div>
-                            <a href="https://hidden-bastion-49542.herokuapp.com/">
-                                <img className="notetaker" src="https://images.ctfassets.net/lzny33ho1g45/5iJ10OKtmYa4BZpYvhb2xw/3d30495ba0fb4603cc957a25193df4fd/best-note-taking-apps-00-hero__2_.png?w=1520&fm=jpg&q=30&fit=thumb&h=760" alt="notetakerapp"></img>
-                            </a>
-                            <h1 class="header4"> Note Taking App </h1>
-                            <p class="link4">- Repository Link: https://github.com/Zenastis/express.js_notetaker </p>   
-                        </div>
-
+                        {
+                            projects.forEach(project => (<div>
+                                <a href={project.codeLink}>
+                                    <img className={project.class} src={project.sourceImage} alt={project.sourceImageAlt} />
+                                </a>
+                                <h1 class="header2">
+                                    {project.description}
+                                </h1>
+                                <Button href={project.repoLink}>Code @ GitHub</Button>
+                            </div>))
+                        }
                     </div>
                 </div>
             </div>
@@ -52,17 +50,4 @@ function Projects () {
     )
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-export default Projects
+export default Projects;
