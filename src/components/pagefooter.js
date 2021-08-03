8070a063e8b50c8afdd730e51f2995d646cf2f05
samuel mcswain <samuelmcswain@hotmail.com>
2021-08-02 20:10:02 -0400

    added resume link

--- src/components/pagefooter.js
+++ src/components/pagefooter.js
@@ -11,7 +11,10 @@ function Pagefooter() {
                 <a href="https://www.linkedin.com/in/samuel-mcswain-8009b0202/"> Linkedin </a>
 
                 <a href="https://github.com/Zenastis"> Github </a>
+
+                <a href="https://docs.google.com/document/d/1MKu2_a5SBKUtuDIG6G6J4fllH4OlitZMDHWK7oMx6-o/edit?usp=sharing"> Resume </a>
             </Row>
+            
 
         </div>
     )
